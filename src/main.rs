f5dc931f1a1061715f1b491ece1d374afc986e1c
David Lum/./Affiliates/Samsung Electronics <david.lum91@gmail.com>
2021-03-08 20:41:31 -0500

    Add coloured output

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod test;
 extern crate cpal;
 
 use cpal::traits::{HostTrait, DeviceTrait, StreamTrait};
-
+use std::io::{self, Write};
+use std::{thread, time};
+use colored::{ColoredString, Colorize};
 
 const MIN_FREQ: usize = 50;
 const MAX_FREQ: usize = 500;
@@ -17,10 +19,11 @@ const MID_ARRAY: usize = ((ARRAY_SIZE / 2) - 1) as usize ;
 const MID_POS: usize = (BUFF_SIZE / 2) as usize;
 
 struct Config {
+    amp_threshold: f32,
+    tuning: f32,
     nbits: usize,
     samples_per_second: usize,
     min_period: usize,
-    max_period: usize,
     buff_size: usize,
     array_size: usize,
     mid_array: usize,
@@ -28,17 +31,161 @@ struct Config {
 }
 
 const CONFIG: Config = Config{
+    amp_threshold: -50.0,
+    tuning: 444.0,
     nbits: NBITS,
     samples_per_second: SAMPLES_PER_SECOND,
     min_period: MIN_PERIOD,
-    max_period: MAX_PERIOD,
     buff_size: BUFF_SIZE,
     array_size: ARRAY_SIZE,
     mid_array: MID_ARRAY,
     mid_pos: MID_POS
 };
 
-const NOTES: [&str; 12] = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const GREETING: &str = r#"
+ *******  **           **     **    **   ********** **      ** ********   ********** **      ** ** ****     **   ********
+/**////**/**          ****   //**  **   /////**/// /**     /**/**/////   /////**/// /**     /**/**/**/**   /**  **//////**
+/**   /**/**         **//**   //****        /**    /**     /**/**            /**    /**     /**/**/**//**  /** **      //
+/******* /**        **  //**   //**         /**    /**********/*******       /**    /**********/**/** //** /**/**
+/**////  /**       **********   /**         /**    /**//////**/**////        /**    /**//////**/**/**  //**/**/**    *****
+/**      /**      /**//////**   /**         /**    /**     /**/**            /**    /**     /**/**/**   //****//**  ////**
+/**      /********/**     /**   /**         /**    /**     /**/********      /**    /**     /**/**/**    //*** //********
+//       //////// //      //    //          //     //      // ////////       //     //      // // //      ///   ////////
+"#;
+
+const C: &str = r#"
+   ******
+  **////**
+ **    //
+/**
+/**
+//**    **
+ //******
+  //////
+"#;
+
+const C_SHARP: &str = r#"
+   ******
+  **////**   **    **
+ **    //  ************
+/**       ///**////**/
+/**         /**   /**
+//**    ** ************
+ //****** ///**////**/
+  //////    //    //
+"#;
+
+const D: &str = r#"
+ *******
+/**////**
+/**    /**
+/**    /**
+/**    /**
+/**    **
+/*******
+///////
+"#;
+
+const D_SHARP: &str = r#"
+ *******
+/**////**    **    **
+/**    /** ************
+/**    /**///**////**/
+/**    /**  /**   /**
+/**    **  ************
+/*******  ///**////**/
+///////     //    //
+"#;
+
+const E: &str = r#"
+ ********
+/**/////
+/**
+/*******
+/**////
+/**
+/********
+////////
+"#;
+
+const F: &str = r#"
+ ********
+/**/////
+/**
+/*******
+/**////
+/**
+/**
+//
+"#;
+
+const F_SHARP: &str = r#"
+ ********
+/**/////    **    **
+/**       ************
+/******* ///**////**/
+/**////    /**   /**
+/**       ************
+/**      ///**////**/
+//         //    //
+"#;
+
+const G: &str = r#"
+   ********
+  **//////**
+ **      //
+/**
+/**    *****
+//**  ////**
+ //********
+  ////////
+"#;
+
+const G_SHARP: &str = r#"
+   ********
+  **//////**   **    **
+ **      //  ************
+/**         ///**////**/
+/**    *****  /**   /**
+//**  ////** ************
+ //******** ///**////**/
+  ////////    //    //
+"#;
+
+const A: &str = r#"
+     **
+    ****
+   **//**
+  **  //**
+ **********
+/**//////**
+/**     /**
+//      //
+"#;
+
+const A_SHARP: &str = r#"
+     **
+    ****      **    **
+   **//**   ************
+  **  //** ///**////**/
+ **********  /**   /**
+/**//////** ************
+/**     /**///**////**/
+//      //   //    //
+"#;
+
+const B: &str = r#"
+ ******
+/*////**
+/*   /**
+/******
+/*//// **
+/*    /**
+/*******
+///////
+"#;
+
+const NOTES: [&str; 12] = [C, C_SHARP, D, D_SHARP, E, F, F_SHARP, G, G_SHARP, A, A_SHARP, B];
 
 struct Bitstream {
     bits: [u32; ARRAY_SIZE]
@@ -65,8 +212,6 @@ impl ZeroCross {
     }
 }
 
-
-
 /// Calculate the smallest power of 2 greater than n.
 /// Useful for getting the appropriate buffer size
 const fn get_smallest_pow2(n: usize) -> usize {
@@ -86,6 +231,11 @@ impl Bitstream {
     //     self.bits.iter_mut().for_each(|x| *x = 0)
     // }
 
+    // fn get(&self, i: usize) -> bool {
+    //     let mask = 1 << (i % CONFIG.nbits);
+    //     (self.bits[i / CONFIG.nbits] & mask) != 0
+    // }
+
     fn set(&mut self, i: usize, val: bool) {
         // Gets the section of 32 bits
         // where i resides
@@ -99,11 +249,6 @@ impl Bitstream {
         // All zeros is identity element with XOR
         let id= if val { u32::MAX } else { 0 };
         *bs ^= (id ^ *bs) & mask;
-    }
-
-    fn get(&self, i: usize) -> bool {
-        let mask = 1 << (i % CONFIG.nbits);
-        (self.bits[i / CONFIG.nbits] & mask) != 0
     }
 
     fn autocorrelate(&self, start_pos: usize) -> (u32, usize, [u32; CONFIG.mid_pos]) {
@@ -174,7 +319,7 @@ impl Bitstream {
         return est_index as usize
     }
 
-    fn estimate_pitch_with_index(signal: &Vec<f32>, est_index: usize) -> Option<f32> {
+    fn estimate_pitch_with_index(signal: &[f32], est_index: usize) -> Option<f32> {
         if est_index >= CONFIG.buff_size {
             return None
         }
@@ -210,7 +355,7 @@ impl Bitstream {
         Some(CONFIG.samples_per_second as f32 / n_samples)
     }
 
-    fn estimate_pitch(signal: &Vec<f32>) -> Option<f32> {
+    fn estimate_pitch(signal: &[f32]) -> Option<f32> {
         let mut zc = ZeroCross::new();
         let mut bs = Bitstream::new();
         for i in 0..CONFIG.buff_size {
@@ -218,7 +363,7 @@ impl Bitstream {
         }
         let (count, est_index, mut corr) = bs.autocorrelate(CONFIG.min_period);
         let est_index = Bitstream::handle_harmonics(count, est_index, &mut corr);
-        Bitstream::estimate_pitch_with_index(&signal, est_index)
+        Bitstream::estimate_pitch_with_index(signal, est_index)
     }
 }
 
@@ -226,13 +371,22 @@ fn linear_to_db(freq: f32) -> f32 {
     20.0 * freq.abs().log10()
 }
 
-fn freq_to_note<'a>(freq: f32) -> (&'a str, f32) {
-    let note_with_cents = 12.0 * (freq / 440.0).log2() + 69.0;
-    let note = (note_with_cents.round() as u32 % 12) as f32;
-    if note_with_cents > note {
-        (NOTES[note as usize], (note_with_cents - note))
-    } else {
-        (NOTES[note as usize], (note - note_with_cents))
+fn freq_to_note<'a>(freq: f32) -> (&'a str, i32) {
+    let note_with_cents = 12.0 * (freq / CONFIG.tuning).log2() + 69.0;
+    let midi_note = note_with_cents.round();
+    let target_freq = 2.0f32.powf((midi_note - 69.0) / 12.0) * CONFIG.tuning;
+    let note = NOTES[midi_note as usize % 12];
+    let cents = (1200.0 * (freq / target_freq).log2()).round() as i32;
+    (note, cents)
+}
+
+fn cents_to_color(note: &str, cents: i32) -> ColoredString {
+    match cents {
+        i32::MIN..=-31 => note.red(),
+        -30..=-9       => note.yellow(),
+        -10..=10       => note.green(),
+        11..=30        => note.yellow(),
+        31..=i32::MAX  => note.red()
     }
 }
 
@@ -249,24 +403,31 @@ fn main() {
         .config();
 
     let mut signal = Vec::with_capacity(CONFIG.buff_size);
+    print!("\x1B[2J\x1B[1;1H");
+    print!("{}", GREETING.red());
 
     let stream = device.build_input_stream(
         &config,
         move |data: &[f32], _: &cpal::InputCallbackInfo| {
 
-            // println!("{:?}", data);
             for d in data.iter() {
                 signal.push(*d);
             }
             if signal.len() >= CONFIG.buff_size {
-                let est_freq = Bitstream::estimate_pitch(&signal);
-                est_freq.map(|f| {
-                    let (note, cents) = freq_to_note(f);
-                    println!("Note is: {}, cents is {:}", note, cents);
-                });
+                let slice = &signal[0..CONFIG.buff_size];
+                let avg: f32 = slice.iter().fold(0.0, |x, y| x + y) / CONFIG.buff_size as f32;
+                if linear_to_db(avg) > CONFIG.amp_threshold {
+                    let est_freq = Bitstream::estimate_pitch(slice);
+                    est_freq.map(|f| {
+                        let (note, cents) = freq_to_note(f);
+                        print!("\x1B[2J\x1B[1;1H");
+                        print!("{}", cents_to_color(note, cents));
+                        io::stdout().flush().unwrap();
+                        thread::sleep(time::Duration::from_millis(100));
+                    });
+                }
                 signal.clear();
             }
-
         },
         move |err| {
             panic!(err);
@@ -275,11 +436,6 @@ fn main() {
     ).unwrap();
 
     stream.play().unwrap();
-    loop {
-        // match receiver.recv() {
-        //     Ok(t) => print!("{:?}", t),
-        //     Err(e) => panic!(e)
-        // }
-    }
+    loop {}
 }
 
